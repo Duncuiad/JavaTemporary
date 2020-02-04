f1222150c8bd10c3c67c1a284467ad5e4509709c
Duncuiad <36989446+Duncuiad@users.noreply.github.com>
2020-02-04 18:25:50 +0100

    Aggiunto un costruttore

--- Cinema.java
+++ Cinema.java
@@ -1,9 +1,24 @@
 public class Cinema {
 
+	private static int contaCinema = 0;
 	private String name;
+
+	/** Costruttore di cinema
+	 *	costruisce l'istanza
+	 * @param nome Il nome del cinema
+	 * @param prezzoBiglietto Il prezzo del biglietto
+	 */
 
 	public Cinema(String nome, double prezzoBiglietto) {
 		this.nome = nome;
 	}
 
+	public Cinema(double prezzoBiglietto) {
+		contaCinema++;
+		this("Cinema_" + contaCinema, prezzoBiglietto);
+	}
+
 }
+/**
+ *
+ */
